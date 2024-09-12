5b928fd18e5cc3ef76099769a2dda217f9f23a23
nitzanavidan <nitzan.avidan@gmail.com>
2024-09-12 10:50:38 +0300

    update dim and calculation method (append)  of  risks and sizes

--- core/eval_utils_masked.py
+++ core/eval_utils_masked.py
@@ -7,13 +7,15 @@ def get_rcps_metrics(lower_bound, upper_bound, ground_truth, masks):
   risks_losses = []
   sizes = []
   for idx in range(upper_bound.shape[0]):
-    risks_losses = risks_losses + [fraction_missed_loss(lower_bound[idx], upper_bound[idx], ground_truth[idx],masks[idx],only_avg_masked=True, avg_channels=False).unsqueeze(dim=0),]
+    risks_losses.append(fraction_missed_loss(lower_bound[idx].unsqueeze(0), upper_bound[idx].unsqueeze(0), ground_truth[idx].unsqueeze(0),masks[idx].unsqueeze(0),only_avg_masked=True, avg_channels=False).item())
     mask_indices = torch.argwhere(masks[idx] == 1.)
-    sizes = sizes + [(upper_bound[idx] - lower_bound[idx])[mask_indices[:, 0], mask_indices[:, 1], mask_indices[:, 2]].mean().unsqueeze(dim=0), ]
+    sizes.append((upper_bound[idx] - lower_bound[idx])[mask_indices[:, 0]].mean().unsqueeze(dim=0).item())
 
 
-  risks_losses = torch.cat(risks_losses, dim=0)
-  sizes = torch.cat(sizes, dim=0)
+  # risks_losses = torch.stack(risks_losses, dim=0)
+  # sizes = torch.stack(sizes, dim=0)
+  risks_losses = torch.tensor(risks_losses).to(lower_bound.device)
+  sizes =torch.tensor(sizes).to(lower_bound.device)
   sizes = sizes + torch.rand(size=sizes.shape,device=sizes.device) * 1e-6
 
   size_bins = torch.tensor([0, torch.quantile(sizes, 0.25), torch.quantile(sizes, 0.5), torch.quantile(sizes, 0.75)], device=sizes.device)
